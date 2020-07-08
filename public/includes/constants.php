9b1f0e22c3940c6672b09ead2753d307885ce348
rodolforodriguezg3 <40470633+rodolforodriguez@users.noreply.github.com>
2020-07-08 01:08:31 -0500

    servicio para actualizar camaras

--- public/includes/constants.php
+++ public/includes/constants.php
@@ -3,6 +3,6 @@
 // Database Constants
 define("DB_SERVER", "127.0.0.1");
 define("DB_USER", "root");
-define("DB_PASS", '123456');
+define("DB_PASS", '');
 define("DB_NAME", "xtamdb");
 define("HOST", "http://" . $_SERVER['SERVER_ADDR'] . "/xtamvideo");
---
+++ public/register.php
@@ -0,0 +1,61 @@
+<?php
+require("includes/connection.php");
+
+    //variables POST
+
+    $estado = $_GET['estado'];
+    $ipserver = $_GET['ipserver'];
+    $channel = $_GET['channel'];
+    
+
+        $select="SELECT estado,cameraid FROM cameras c inner join centro_comercial cc on cc.id=c.id_centrocomercial
+            where dcamara='".$channel."' and ipserver='".$ipserver."'";
+            
+        $regitro=mysqli_query($con, $select);
+        $valor=mysqli_fetch_assoc($regitro);
+        $valorvali=mysqli_num_rows($regitro);
+        
+        if($valorvali > 0){
+            if($valor['estado']!=$estado){
+                echo "entro";
+                $consulta = "UPDATE cameras SET estado='" . $estado . "' WHERE cameraid=" . $valor['cameraid'];
+                echo $consulta;
+                $add = mysqli_query($con, $consulta);
+                if ($add) {
+                    echo "estado actualizado";
+                } else {
+                    echo "intente de nuevo";
+                }
+
+                $consulta = "INSERT INTO rtsp_log(`name_channel`, `ip_remote`, `status`) 
+                VALUES ('" . $channel . "','" . $ipserver . "','" . $estado . "')";
+                    //echo $consulta;
+                $register = mysqli_query($con, $consulta);
+                if ($register) {
+                    echo "Log Insertado";
+                } else {
+                    echo "intente de nuevo";
+                }
+    
+    
+            }else{
+                $select="SELECT * FROM rtsp_log
+                    where name_channel='".$channel."' and ip_remote='".$ipserver."'";
+            
+                $regitro=mysqli_query($con, $select);
+                $valor=mysqli_fetch_assoc($regitro);
+                $valorvali=mysqli_num_rows($regitro);
+                if($valorvali == 0){
+                $consulta = "INSERT INTO rtsp_log(`name_channel`, `ip_remote`, `status`) 
+                VALUES ('" . $channel . "','" . $ipserver . "','" . $estado . "')";
+                $register = mysqli_query($con, $consulta);
+                if ($register) {
+                    echo "Log Insertado";
+                } else {
+                    echo "intente de nuevo";
+                }
+
+                }
+        
+            }
+        }
